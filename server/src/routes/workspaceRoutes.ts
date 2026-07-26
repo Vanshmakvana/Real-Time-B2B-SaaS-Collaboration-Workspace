f8ef413bab9705e66a1e4a5414300a5e39576173
Prathmesh M <163981164+Prath2244@users.noreply.github.com>
2026-07-26 12:24:57 +0530

    Update workspaceRoutes.ts

--- server/src/routes/workspaceRoutes.ts
+++ server/src/routes/workspaceRoutes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-
 import protect from "../middleware/authMiddleware";
 
 import {
@@ -10,21 +9,29 @@ import {
   deleteWorkspace,
   joinWorkspace,
   regenerateInviteCode,
+  searchWorkspaces,
 } from "../controllers/workspaceController";
 
 const router = Router();
 
 router.use(protect);
 
+// Create a workspace & Get all workspaces
 router
   .route("/")
   .post(createWorkspace)
   .get(getMyWorkspaces);
 
+// Search workspaces
+router.get("/search", searchWorkspaces);
+
+// Join a workspace using an invite code
 router.post("/join", joinWorkspace);
 
+// Regenerate invite code
 router.put("/:id/invite", regenerateInviteCode);
 
+// Workspace CRUD operations
 router
   .route("/:id")
   .get(getWorkspaceById)
