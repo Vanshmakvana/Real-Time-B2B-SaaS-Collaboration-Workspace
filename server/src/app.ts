0c948e8db3cad08566668d89d91862d838e72cdd
Prathmesh M <163981164+Prath2244@users.noreply.github.com>
2026-07-30 10:52:24 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,36 @@
 import express from "express";
 import cors from "cors";
-import workspaceRoutes from "./routes/workspaceRoutes";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
 import authRoutes from "./routes/authRoutes";
+import workspaceRoutes from "./routes/workspaceRoutes";
+import channelRoutes from "./routes/channelRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import socketRoutes from "./routes/socketRoutes";
+
+import notFound from "./middleware/notFound";
 import errorHandler from "./middleware/errorMiddleware";
 
 const app = express();
 
-app.use(cors());
+app.use(helmet());
 
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: {
+      success: false,
+      message: "Too many requests. Please try again later.",
+    },
+  })
+);
+
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (_, res) => {
+app.get("/", (_req, res) => {
   res.json({
     success: true,
     message: "API Running",
@@ -19,6 +39,11 @@ app.get("/", (_, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/workspaces", workspaceRoutes);
+app.use("/api/channels", channelRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/socket", socketRoutes);
+
+app.use(notFound);
 app.use(errorHandler);
 
 export default app;
