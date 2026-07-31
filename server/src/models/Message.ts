21fbedc45da54cdb41a423acafe6ef4f0935c5b0
Vansh Makvana <132293663+Vanshmakvana@users.noreply.github.com>
2026-07-31 19:19:30 +0530

    Update Message.ts

--- server/src/models/Message.ts
+++ server/src/models/Message.ts
@@ -28,7 +28,7 @@ const messageSchema = new Schema<IMessage>(
   {
     timestamps: true,
     toJSON: {
-      transform: (doc, ret) => {
+      transform: (doc, ret: any) => {
         ret.id = ret._id.toString();
         delete ret._id;
         delete ret.__v;
