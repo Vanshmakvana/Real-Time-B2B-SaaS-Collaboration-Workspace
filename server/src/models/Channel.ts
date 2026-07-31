134c493cba211954595114e1d735ae4ac256f37b
Vansh Makvana <132293663+Vanshmakvana@users.noreply.github.com>
2026-07-31 19:19:10 +0530

    Update Channel.ts

--- server/src/models/Channel.ts
+++ server/src/models/Channel.ts
@@ -27,7 +27,7 @@ const channelSchema = new Schema<IChannel>(
   {
     timestamps: true,
     toJSON: {
-      transform: (doc, ret) => {
+      transform: (doc, ret: any) => {
         ret.id = ret._id.toString();
         delete ret._id;
         delete ret.__v;
