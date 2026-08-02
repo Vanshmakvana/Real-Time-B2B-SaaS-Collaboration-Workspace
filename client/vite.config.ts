764829510a779e9d949a63e32bab2fc8287ea855
Vansh Makvana <132293663+Vanshmakvana@users.noreply.github.com>
2026-08-02 19:29:35 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,2 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
-});
+declare const _default: import("vite").UserConfig;
+export default _default;
